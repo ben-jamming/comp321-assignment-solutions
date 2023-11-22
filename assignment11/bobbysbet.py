4770e95a0e3c9e3d729b6ed1dff3ce60a2ca9702
BenHepditch <benhepditch@gmail.com>
2023-11-22 15:08:53 -0500

    finish bobbysbet

--- assignment11/bobbysbet.py
+++ assignment11/bobbysbet.py
@@ -1,9 +1,11 @@
 import math
 
 def good_bet(r,s,x,y,w) -> bool:
-    p = s-r+1
-    p_x = (math.comb(y,x) + p**x + (1-p)**(y-x)) * w
-    return p_x >= 1
+    p = (s-r+1)/s
+    p_x = 0
+    for i in range(x,y+1):
+        p_x += math.comb(y,i)* (p**i) * ((1-p)**(y-i))
+    return p_x * w > 1
     
 
 def main():
